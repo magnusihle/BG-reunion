815bc1036d9e6a2f50a6170bfb2e0c5e91b00741
magnusihle <magnus@ihle.no>
2023-05-03 22:58:14 +0200

    Final changes. Added external webhook.

--- pages/canceled.tsx
+++ pages/canceled.tsx
@@ -14,13 +14,15 @@ const CanceledPage = () => {
   }, [router]);
 
   return (
-    <div className="mx-auto max-w-7xl px-6 bg-blue justify-center h-screen py-32 text-center sm:py-40 lg:px-8 relative">
-      <h1 className="text-2xl text-beige font-bold uppercase">
-        Kjøpet ditt ble kansellert
-      </h1>
-      <p className="mt-12 text-beige uppercase">
-        Du vil bli omdirigert til hjemmesiden om kort tid
-      </p>
+    <div className="bg-blue h-screen w-screen">
+      <div className="flex flex-col mx-auto max-w-7xl px-6 bg-blue justify-center h-full py-32 text-center sm:py-40 lg:px-8 relative">
+        <h1 className="text-2xl text-beige font-bold uppercase">
+          Kjøpet ditt ble kansellert
+        </h1>
+        <p className="mt-12 text-beige uppercase">
+          Du vil bli omdirigert til hjemmesiden om kort tid
+        </p>
+      </div>
     </div>
   );
 };
--- pages/success.tsx
+++ pages/success.tsx
@@ -14,10 +14,12 @@ const SuccessPage = () => {
   }, [router]);
 
   return (
-    <div className="mx-auto max-w-7xl px-6 bg-blue justify-center h-screen py-32 text-center sm:py-40 lg:px-8 relative">
-      <h1 className="text-2xl text-beige font-bold uppercase">
-        Takk for handelen, kjipt å bli scammet!
-      </h1>
+    <div className="bg-blue h-screen w-screen">
+      <div className="flex flex-col mx-auto max-w-7xl px-6 bg-blue justify-center h-full py-32 text-center sm:py-40 lg:px-8 relative">
+        <h1 className="text-2xl text-beige font-bold uppercase">
+          Takk for handelen, kjipt å bli scammet!
+        </h1>
+      </div>
     </div>
   );
 };
