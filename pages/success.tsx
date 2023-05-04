05441f4ad5bd6e6c725e39eb86f71466a67f3b31
magnusihle <magnus@ihle.no>
2023-05-04 12:43:56 +0200

    Update seccess page

--- pages/success.tsx
+++ pages/success.tsx
@@ -17,7 +17,7 @@ const SuccessPage = () => {
     <div className="bg-blue h-screen w-screen">
       <div className="flex flex-col mx-auto max-w-7xl px-6 bg-blue justify-center h-full py-32 text-center sm:py-40 lg:px-8 relative">
         <h1 className="text-2xl text-beige font-bold uppercase">
-          Takk for handelen, kjipt å bli scammet!
+          Takk for handelen, vi sees!
         </h1>
       </div>
     </div>
